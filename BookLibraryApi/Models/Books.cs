4c49af03d2d1427a6c491fc1d1f7ced058d81924
Daniel Lee <lee.d37@gmail.com>
2025-04-28 14:47:52 -0700

    initial commit

---
+++ BookLibraryApi/Models/Books.cs
@@ -0,0 +1,11 @@
+﻿namespace BookLibraryApi.Models
+{
+    public class Books
+    {
+        public int Id { get; set; } // unique identifier
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string YearPublished { get; set; }
+    }
+}
